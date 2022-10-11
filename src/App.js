5da41a684254056f45314c586dbb8ac3db97d67d
ZikaZaki <zika.github@gmail.com>
2022-10-11 11:43:08 +0300

    Remove unecessary component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import ProfilePage from './Components/ProfilePage/ProfilePage';
-// import Header from './Components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
       <Navbar />
       <Routes>
         <Route path="/" element={<h1>Rockets</h1>} />
