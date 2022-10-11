595df50654c827b7e2ba2cfef3475e016f4e20ec
Saikat Chakraborty <saikatgpae@gmail.com>
2022-10-12 00:51:19 +0530

    Remove linters error.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
