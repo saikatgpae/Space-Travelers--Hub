d2f15463f9df32a319f63b6eeb20eef6a0bc6445
Saikat Chakraborty <saikatgpae@gmail.com>
2022-10-12 00:33:22 +0530

    Remove linters.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,37 +1,20 @@
-import * as React from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import styles from './Navbar.module.css';
-// import the nav image
-import NavLogo from './planet.png';
+import './NavBar.css';
+import logo from './logo.png';
 
-const Navbar = () => (
-  <div className={styles['nav-container']}>
-    <nav className={styles.nav}>
-      <div className={styles['nav-logo']}>
-        <img className={styles['logo-img']} src={NavLogo} alt="planet" />
-        <h1 className={styles['logo-title']}>Space Travelers&apos; Hub</h1>
-      </div>
-      <ul className={styles['nav-list']}>
-        <li className={styles['list-item']}>
-          <NavLink className={styles['nav-link']} to="/" exact="true">
-            Rockets
-          </NavLink>
-        </li>
-        <li className={styles['list-item']}>
-          <NavLink className={styles['nav-link']} to="/missions">
-            Missions
-          </NavLink>
-        </li>
-        <span className={styles.separator}> | </span>
-        <li className={styles['list-item']}>
-          <NavLink className={styles['nav-link']} to="/profile">
-            My Profile
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-    <hr />
-  </div>
-);
-
-export default Navbar;
+export default function NavBar() {
+  return (
+    <div className="header p-4">
+      <img className="logo" src={logo} alt="logo" />
+      <nav className="nav-bar">
+        <h1>Space Travelers&apos; Hub</h1>
+        <ul className="nav-menu">
+          <li><NavLink className="rockets" activeclassname="active" to="/rockets">Rockets</NavLink></li>
+          <li><NavLink className="mission" activeclassname="active" to="/missions">Missions</NavLink></li>
+          <li><NavLink className="my-profile" activeclassname="active" to="/myprofile">My Profile</NavLink></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
