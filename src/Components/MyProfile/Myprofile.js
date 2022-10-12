a5571cf161d561e329d32f657a6ffc2a59dcbba2
Saikat Chakraborty <saikatgpae@gmail.com>
2022-10-12 19:15:58 +0530

    Add basic structure of mission.

--- src/Components/MyProfile/Myprofile.js
+++ src/Components/MyProfile/Myprofile.js
@@ -8,22 +8,26 @@ export default function Myprofile() {
         <div className="col-6">
           <h5 className="col-12 py-3">My Missions</h5>
           <table className="col-12 row">
-            <tr className="col-12 row">
-              <td className="col-12 py-3">Mission Name</td>
-              <td className="col-12 py-3">Mission Name</td>
-              <td className="col-12 py-3">Mission Name</td>
-              <td className="col-12 py-3">Mission Name</td>
-            </tr>
+            <tbody>
+              <tr className="col-12 row">
+                <td className="col-12 py-3">Mission Name</td>
+                <td className="col-12 py-3">Mission Name</td>
+                <td className="col-12 py-3">Mission Name</td>
+                <td className="col-12 py-3">Mission Name</td>
+              </tr>
+            </tbody>
           </table>
         </div>
+        <h5 className="col-12 py-3">My Rockets</h5>
         <table className="col-6">
-          <h5 className="col-12 py-3">My Rockets</h5>
-          <tr className="col-12 row">
-            <td className="col-12 py-3">Rocket Name</td>
-            <td className="col-12 py-3">Rocket Name</td>
-            <td className="col-12 py-3">Rocket Name</td>
-            <td className="col-12 py-3">Rocket Name</td>
-          </tr>
+          <tbody>
+            <tr className="col-12 row">
+              <td className="col-12 py-3">Rocket Name</td>
+              <td className="col-12 py-3">Rocket Name</td>
+              <td className="col-12 py-3">Rocket Name</td>
+              <td className="col-12 py-3">Rocket Name</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
