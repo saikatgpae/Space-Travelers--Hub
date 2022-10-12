8ff4079db836c6b9dadc0cbba2ad04f4794e3a0c
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-10-12 14:56:42 +0300

    Bulid My profile page

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ export default function NavBar() {
         <h1>Space Travelers&apos; Hub</h1>
         <ul className="nav-menu">
           <li><NavLink className="rockets" activeclassname="active" to="/rockets">Rockets</NavLink></li>
-          <li><NavLink className="mission" activeclassname="active" to="/missions">Missions</NavLink></li>
+          <li><NavLink className="mission " activeclassname="active" to="/missions">Missions</NavLink></li>
           <li><NavLink className="my-profile" activeclassname="active" to="/myprofile">My Profile</NavLink></li>
         </ul>
       </nav>
