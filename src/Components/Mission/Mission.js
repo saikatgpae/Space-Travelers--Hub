8578a196d6ed2ba403acf0e36c55498a796bbe9d
Saikat Chakraborty <saikatgpae@gmail.com>
2022-10-14 16:15:21 +0530

    Toggle of the status.

--- src/Components/Mission/Mission.js
+++ src/Components/Mission/Mission.js
@@ -23,7 +23,11 @@ export default function Mission(props) {
         <td className="mission-info p-3">
           {description}
         </td>
-        <td className="p-2 text-center"><small className={joined ? 'mission-status active-member p-1' : 'mission-status inactive-member p-1'}>NOT A MEMBER</small></td>
+        <td className="p-2 text-center">
+          <small className={joined ? 'mission-status active-member p-1' : 'mission-status inactive-member p-1'}>
+            {joined ? 'Active Member' : 'NOT A MEMBER'}
+          </small>
+        </td>
         <td className="mission-join p-3">
           <button className={joined ? 'join-button inactive' : 'join-button'} onClick={(event) => handelClick(id, event)} type="button">
             {joined ? 'Leave Mission' : 'Join Mission'}
